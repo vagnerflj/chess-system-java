18a3f01d685d08ae0cd09896e98dddc722985b72
Vagner ferreira <euvagnerferreira@gmail.com>
2024-04-01 08:23:41 -0300

    First class: Position

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,8 +1,11 @@
 package Application;
 
+import boardgame.Position;
+
 public class Program {
     public static void main(String[] args){
 
-        System.out.println("hello word!");
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
     }
 }
